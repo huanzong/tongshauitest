8b4509c394f544bc7c44b90fcb3fc79f6dd151ec
许一红 <xu.yihong@trs.com.cn>
2017-08-22 15:26:47 +0800

    统帅登录

--- leader_site/images/login.js
+++ leader_site/images/login.js
@@ -7,24 +7,43 @@ $(function() {
     });
 
     function init() {
+        //默认电脑登陆
+        $('.js_loginType_pc').show();
+        $('.js_loginType_phone').hide();
+
         var screenWidth = document.body.offsetWidth;
         var screenHeight = document.body.offsetHeight;
         var loginHeight = $('.js_center').height();
 
-        if (screenWidth <= 767) {
+        if (screenWidth <= 700) {
             
-        } else if (screenWidth > 767 && screenWidth <= 992) {
-            
-        } else if (screenWidth > 992 && screenWidth <= 1190) {
-            
-        } else if (screenWidth > 1190) {
-            
+        } else {
+            setTimeout(function() {
+                $('.js_center').css('margin-top',((screenHeight-loginHeight)/2-20)+'px');
+            }, 10);
         }
 
-        setTimeout(function() {
-            $('.js_center').css('margin-top',((screenHeight-loginHeight)/2-20)+'px');
-        }, 10);
+        
+
     }
+
+    // 登录方式转换
+    $('.js_loginType').on('click',function(){
+        var phoneType = $(this);
+        if(phoneType.prop('data-login')){
+            $('.js_loginType_tab').removeClass('icon-phone').addClass('icon-computer');
+            $('.js_loginType_pc').show();
+            $('.js_loginType_phone').hide();
+            $('.js_loginType_tit').text('登录统帅');
+            phoneType.prop('data-login',0);
+        }else{
+            $('.js_loginType_tab').removeClass('icon-computer').addClass('icon-phone');
+            $('.js_loginType_phone').show();
+            $('.js_loginType_pc').hide();
+            $('.js_loginType_tit').text('动态密码登录');
+            phoneType.prop('data-login',1);
+        }
+    });
 
 });
 
