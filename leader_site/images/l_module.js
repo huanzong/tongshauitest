33288e7785209be8a1111548ff32eca07196f62c
许一红 <xu.yihong@trs.com.cn>
2017-08-23 10:07:35 +0800

    公共

---
+++ leader_site/images/l_module.js
@@ -0,0 +1,47 @@
+$(function() {
+
+    init();
+
+    $(window).resize(function() {
+        init();
+    });
+
+    function init() {
+        
+
+    }
+
+    //输入框placeholder支持ie8
+    $('input').each(function(){
+        var $this =$(this);
+        var text = $this.attr("ph");
+        if (text) {
+            if ($this.val() == "") {
+                $this.val(text);
+                $this.css('color','#ccc');
+            }
+        }
+        if($this.attr("type") == "text"){
+            if (!$this.attr('ph')){
+                return;
+            }
+            $this.blur(function () {
+                if($this.val() == ''){
+                    $this.val($this.attr("ph"));
+                    $this.css('color','#ccc');
+                }else{
+                    $this.css('color','#666');
+                }
+            }).focus(function () {
+                if($this.val() == $this.attr("ph")){
+                    $this.val("");
+                    $this.css('color','#666');
+                }
+            });
+        }
+    });
+
+});
+
+
+
---
+++ leader_site/images/registration.js
@@ -0,0 +1,51 @@
+$(function() {
+
+    init();
+
+    $(window).resize(function() {
+        init();
+    });
+
+    function init() {
+        //默认电脑登陆
+        $('.js_loginType_pc').show();
+        $('.js_loginType_phone').hide();
+
+        var screenWidth = document.body.offsetWidth;
+        var screenHeight = document.body.offsetHeight;
+        var loginHeight = $('.js_center').height();
+
+        if (screenWidth <= 700) {
+            setTimeout(function() {
+                $('.js_center').css('margin-top',0);
+            }, 10);
+        } else {
+            setTimeout(function() {
+                $('.js_center').css('margin-top',((screenHeight-loginHeight)/2-20)+'px');
+            }, 10);
+        }
+
+    }
+
+    // 登录方式转换
+    $('.js_loginType').on('click',function(){
+        var phoneType = $(this);
+        if(phoneType.prop('data-login')){
+            $('.js_loginType_tab').removeClass('icon-phone').addClass('icon-computer');
+            $('.js_loginType_pc').show();
+            $('.js_loginType_phone').hide();
+            $('.js_loginType_tit').text('登录统帅');
+            $('.js_loginType').prop('data-login',0);
+        }else{
+            $('.js_loginType_tab').removeClass('icon-computer').addClass('icon-phone');
+            $('.js_loginType_phone').show();
+            $('.js_loginType_pc').hide();
+            $('.js_loginType_tit').text('动态密码登录');
+            $('.js_loginType').prop('data-login',1);
+        }
+    });
+
+});
+
+
+
