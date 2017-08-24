a4e43db0f952ca54740c2ebd2feb182b1396370f
许一红 <xu.yihong@trs.com.cn>
2017-08-24 17:21:02 +0800

    ui

--- leader_site/images/ui.js
+++ leader_site/images/ui.js
@@ -13,9 +13,14 @@ $(function() {
 
     }
     //简易导航栏
-    $('.js_navShow').siblings('a').hide();
+    $('.js_navShow').siblings('li').fadeOut();
     $('.js_navShow').on('click',function(){
-
+        if($(this).siblings('li').eq(0).css('display')=='none'){
+            $(this).siblings('li').fadeIn();
+        }else{
+            $(this).siblings('li').fadeOut();
+        }
+        
     });
 
     //checkbox，radio样式初始化
