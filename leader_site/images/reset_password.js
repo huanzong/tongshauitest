d512d8d5c2ef1fd403e7c0fce425f25f514f2082
duqian <du.qian@trs.com.cn>
2017-08-24 15:10:22 +0800

    下拉菜单

--- leader_site/images/reset_password.js
+++ leader_site/images/reset_password.js
@@ -24,34 +24,94 @@ $(function() {
         }
 
         //密码进入事件,更改密码框类型为密码
-        $("#login_password2").focus(function () {
+        $("#js_loginPassword2").focus(function () {
             $(this).addClass("o_df-hide");
-            $("#login_password").removeClass("o_df-hide").focus();
+            $("#js_loginPassword").removeClass("o_df-hide").focus();
         });
         //密码离开事件,更改密码框类型为文本
-        $("#login_password").blur(function () {
+        $("#js_loginPassword").blur(function () {
             var password = $.trim($(this).val());
             if( "" == password || "6-16位，数字，字母或符合的组合" == password || null == password){
-                $("#login_password").addClass("o_df-hide");
-                $("#login_password2").removeClass("o_df-hide")
+                $("#js_loginPassword").addClass("o_df-hide");
+                $("#js_loginPassword2").removeClass("o_df-hide")
             }
         });
 
-        //倒计时
-        sendTime();
-        var wait = 5;
-        function sendTime() {
-            if (wait == 0) {
-                //window.location.href =""
-                wait = 5;
-            } else {
 
-                $("#js_time").html(wait);
-                wait--;
-                var activecodetime= setTimeout(function() {
-                    sendTime()
-                },1000)
+
+        //判断为空
+        function isNull(val){
+            if(!val){
+                return true;
             }
+            if(val == null || val == "" || val == "undefined" || val == "null" || val == "NULL"){
+                return true;
+            }
+            return false;
+        }
+        //第一步验证
+        $("#js_resetCode,#js_resetName").keyup(function () {
+            var resetName = $.trim($("#js_resetName").val());
+            var resetNamePh = $.trim($("#js_resetName").attr("ph"));
+            var resetCode = $.trim($("#js_resetCode").val());
+            var resetCodePh = $.trim($("#js_resetCode").attr("ph"));
+            if(!isNull(resetName) && resetName != resetNamePh && !isNull(resetCode) && resetCode != resetCodePh){
+                $(".js_resetFirst").removeClass("l-btn-disable");
+            }
+        });
+        $(".js_resetFirst").on('click',function () {
+            if(!$(this).hasClass("l-btn-disable")) {
+                $(".js_firstStep").addClass("o_df-hide");
+                $(".js_twoStep").removeClass("o_df-hide");
+            }
+        })
+        //第二步验证
+        $("#js_resetCode2").keyup(function () {
+            var resetCode2 = $.trim($("#js_resetCode2").val());
+            var resetCodePh2 = $.trim($("#js_resetCode2").attr("ph"));
+            if(!isNull(resetCode2) && resetCode2 != resetCodePh2){
+                $(".js_resetTwo").removeClass("l-btn-disable");
+            }
+        });
+        $(".js_resetTwo").on('click',function () {
+            if(!$(this).hasClass("l-btn-disable")) {
+                $(".js_twoStep").addClass("o_df-hide");
+                $(".js_threeStep").removeClass("o_df-hide");
+            }
+        })
+        //第三步验证
+        $("#js_loginPassword").keyup(function () {
+            var loginPassword = $.trim($("#js_loginPassword").val());
+            var loginPasswordPh = $.trim($("#js_loginPassword").attr("ph"));
+            if(!isNull(loginPassword) && loginPassword != loginPasswordPh){
+                $(".js_resetFinish").removeClass("l-btn-disable");
+            }
+        });
+        $(".js_resetFinish").on('click',function () {
+            if(!$(this).hasClass("l-btn-disable")) {
+                $(".js_resetboxForm").addClass("o_df-hide");
+                $(".js_resetboxFormSuccess").removeClass("o_df-hide");
+                sendTime();//倒计时
+            }
+        })
+
+
+    }
+     $("#js_selectValidate").oSelect().init();//下拉菜单初始化
+
+    //倒计时
+    var wait = 5;
+    function sendTime() {
+        if (wait == 0) {
+            //window.location.href =""
+            wait = 5;
+        } else {
+
+            $("#js_time").html(wait);
+            wait--;
+            var activecodetime= setTimeout(function() {
+                sendTime()
+            },1000)
         }
     }
 })
