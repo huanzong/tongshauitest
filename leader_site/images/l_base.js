df054c21df6efc53350e1d326d64a9c646eeb213
许一红 <xu.yihong@trs.com.cn>
2017-08-31 18:33:00 +0800

    导航

--- leader_site/images/l_base.js
+++ leader_site/images/l_base.js
@@ -14,15 +14,28 @@ $(function() {
     /**
      * 导航逻辑
      */
+    
     //导航栏目显示隐藏
     $('.js_column').on('hover',function(){
         $('.js_column_show').show();
     });
     $('.js_column_show').on('mouseleave',function(){
-        $(this).hide();
+        // $(this).hide();
+    });
+
+    //搜索历史
+    $('.js_searchHistory').bind('input propertychange',function(){
+        if($(this).val()){
+            $('.js_searchBox').show();
+        }else{
+            $('.js_searchBox').hide();
+        }
+    }).on('blur',function(){
+        $('.js_searchBox').hide();
+    }).on('focus',function(){
+        if($(this).val()){
+            $('.js_searchBox').show();
+        }
     });
     
 });
-
-
-
