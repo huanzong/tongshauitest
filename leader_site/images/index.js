9ee3f25df889a421b6ce10a3cc1b0882c5cf73eb
许一红 <xu.yihong@trs.com.cn>
2017-09-27 09:13:55 +0800

    首页

---
+++ leader_site/images/index.js
@@ -0,0 +1,18 @@
+$(function() {
+
+    init();
+
+    $(window).resize(function() {
+        init();
+    });
+
+    function init() {
+
+        var screenWidth = document.body.offsetWidth;
+        var screenHeight = document.body.offsetHeight;
+
+    }
+});
+
+
+
