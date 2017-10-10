e05c529a566cdf10636295f99ea7f491c911a9ab
许一红 <xu.yihong@trs.com.cn>
2017-10-10 16:29:35 +0800

    导航头像

--- leader_site/images/index.js
+++ leader_site/images/index.js
@@ -11,6 +11,7 @@ $(function() {
   }
 
   var swiper = {};//用来存放所有轮播
+  
 	setTimeout(function(){
 
 		//banner轮播
@@ -145,21 +146,21 @@ $(function() {
 
     var screenWidth = document.body.offsetWidth;
     if (screenWidth <= 700) {
-      swiper.entrabceSwiper.params.slidesPerView = 2
-      swiper.entrabceSwiper.params.watchActiveIndex = true
-      swiper.entrabceSwiper.params.updateOnImagesReady = true
+      swiper.entrabceSwiper.params.slidesPerView = 2;
+      swiper.entrabceSwiper.params.watchActiveIndex = true;
+      swiper.entrabceSwiper.params.updateOnImagesReady = true;
     } else {
-      swiper.entrabceSwiper.params.slidesPerView = 1
-      swiper.entrabceSwiper.reInit()
+      swiper.entrabceSwiper.params.slidesPerView = 1;
+      swiper.entrabceSwiper.reInit();
     }
 
 
     if (screenWidth <= 991) {
 
 
-      swiper.bannerSwiper.params.slidesPerView = 3
-      swiper.bannerSwiper.params.watchActiveIndex = true
-      swiper.bannerSwiper.params.updateOnImagesReady = true
+      swiper.bannerSwiper.params.slidesPerView = 3;
+      swiper.bannerSwiper.params.watchActiveIndex = true;
+      swiper.bannerSwiper.params.updateOnImagesReady = true;
       swiper.bannerSwiper.params.onSlideChangeEnd = function(swiper){
           var index = swiper.activeIndex;
           // var imgSrc = $('.js_swiperRecMain .swiper-slide').eq(index).find('img').attr('src');
@@ -170,16 +171,16 @@ $(function() {
             'height': 500,
             'margin-left': -455.5,
             'margin-top': -250
-          })
+          });
 
           $('.js_bannerSwiper .swiper-slide').not('.swiper-slide-active').find('img').css({
             'height': 382,
             'margin-left': -348.5,
             'margin-top': -191
-          })
-        }
+          });
+        };
 
-      swiper.bannerSwiper.reInit()
+      swiper.bannerSwiper.reInit();
       $(".js_bg").oBgCover().init();
 
       setTimeout(function(){
@@ -187,26 +188,26 @@ $(function() {
           'height': 382,
           'margin-left': -348.5,
           'margin-top': -191
-        })
+        });
 
         $('.js_bannerSwiper .swiper-slide-active').find('img').css({
           'height': 500,
           'margin-left': -455.5,
           'margin-top': -250
-        })
-      },500)
+        });
+      },500);
     } else {
-      swiper.bannerSwiper.params.slidesPerView = 1
+      swiper.bannerSwiper.params.slidesPerView = 1;
       swiper.bannerSwiper.params.onSlideChangeEnd = function(swiper){
         var index = swiper.activeIndex;
         // var imgSrc = $('.js_swiperRecMain .swiper-slide').eq(index).find('img').attr('src');
         // $('.js_recImgSmall').attr('src',imgSrc);
-      }
-      swiper.bannerSwiper.reInit()
+      };
+      swiper.bannerSwiper.reInit();
     }
 
 
-    swiper.entrabceSwiper.reInit()
+    swiper.entrabceSwiper.reInit();
 
   });
 });
