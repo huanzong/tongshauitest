73a4c27fb8d6123347bd49cb3d216ef078ead773
潘梦琳 <pan.menglin@trs.com.cn>
2017-10-10 14:15:41 +0800

    调整首页入口

--- leader_site/images/index.js
+++ leader_site/images/index.js
@@ -104,18 +104,30 @@ $(function() {
 	    swiper.recommendSwiper.swipeNext();
 	  });
 
+    //推荐商品图片-响应式
+    $(".js_recPic").each(function(){
+      $(this).oPicture({
+          //自定义节点宽度
+          //sm:544,md:700,lg:992,xl:1200,
+      }).init();
+    });
+
 
     swiper.entrabceSwiper = new Swiper ('.js_swiperEntrabce', {
 
       loop: true,
       // autoplay: 3000,
       calculateHeight : true,
+      slidesPerView: 2,
+      watchActiveIndex: true,
+      updateOnImagesReady: true,
       onSlideChangeEnd: function(swiper){
         var index = swiper.activeIndex;
         var imgSrc = $('.js_swiperEntrabce .swiper-slide').eq(index).find('img').attr('src');
         $('.js_recImgSmall').attr('src',imgSrc);
       }
     });
+
     $('.js_swiperEntrabcePrev').on('click', function(e){
       e.preventDefault();
       swiper.entrabceSwiper.swipePrev();
@@ -126,13 +138,6 @@ $(function() {
     });
 
 
-	  //推荐商品图片-响应式
-	  $(".js_recPic").each(function(){
-      $(this).oPicture({
-          //自定义节点宽度
-          //sm:544,md:700,lg:992,xl:1200,
-      }).init();
-    });
 
 	},2000);
 
@@ -192,6 +197,7 @@ $(function() {
     }
 
 
+    swiper.entrabceSwiper.reInit()
 
   });
 });
