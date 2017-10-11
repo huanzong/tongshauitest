9b6a9de5ee666b60c00cde36b0dc9bf6ab8eea35
许一红 <xu.yihong@trs.com.cn>
2017-10-11 20:25:12 +0800

    列表页筛选项

--- leader_site/images/index.js
+++ leader_site/images/index.js
@@ -12,128 +12,167 @@ $(function() {
 
   var swiper = {};//用来存放所有轮播
   
-	setTimeout(function(){
+	// setTimeout(function(){
 
-		//banner轮播
-    var screenWidth = document.body.offsetWidth;
-    if (screenWidth <= 991) {
-      swiper.bannerSwiper = new Swiper('.js_bannerSwiper',{
-        loop: true,
-        // autoplay: 3000,
-        slidesPerView: 3,
-        watchActiveIndex: true,
-        updateOnImagesReady: true,
-        centeredSlides: true,
-        onSlideChangeEnd: function(swiper){
-          var index = swiper.activeIndex;
-
-          $('.js_bannerSwiper .swiper-slide-active').find('img').css({
-            'height': 500,
-            'margin-left': -455.5,
-            'margin-top': -250
-          });
-
-          $('.js_bannerSwiper .swiper-slide').not('.swiper-slide-active').find('img').css({
-            'height': 382,
-            'margin-left': -348.5,
-            'margin-top': -191
-          });
-        }
-      });
-      $(".js_bg").oBgCover().init();
-
-      $('.js_bannerSwiper .swiper-slide').not('.swiper-slide-active').find('img').css({
-        'height': 382,
-        'margin-left': -348.5,
-        'margin-top': -191
-      });
-
-      $('.js_bannerSwiper .swiper-slide-active').find('img').css({
-        'height': 500,
-        'margin-left': -455.5,
-        'margin-top': -250
-      });
-
-      // swiper.bannerSwiper.
-    } else {
-      swiper.bannerSwiper = new Swiper('.js_bannerSwiper',{
-        loop: true,
-        autoplay: 3000,
-
-        onSlideChangeEnd: function(swiper){
-          var index = swiper.activeIndex;
-          console.log(index);
-        }
-    	});
-    }
-
-
-		setTimeout(function(){
-
-			//banner左侧高度跟随
-	    $('.js_bannerInfo').oHrel({
-	      obj:'$(this).next()'
-	    }).init();
-	    //banner左侧文字居中
-	    $(".js_bannnerCenter").oBoxCenter().init();
-
-		},1000);
-
-
-		//产品推荐轮播
-	  swiper.recommendSwiper = new Swiper ('.js_swiperRecMain', {
-
-	    loop: true,
-	    // autoplay: 3000,
-
-	    onSlideChangeEnd: function(swiper){
-	    	var index = swiper.activeIndex;
-	    	var imgSrc = $('.js_swiperRecMain .swiper-slide').eq(index).find('img').attr('src');
-	      $('.js_recImgSmall').attr('src',imgSrc);
-	    }
-		});
-		$('.js_swiperRecMainPrev').on('click', function(e){
-    	e.preventDefault();
-	    swiper.recommendSwiper.swipePrev();
-	  });
-	  $('.js_swiperRecMainNext').on('click', function(e){
-	    e.preventDefault();
-	    swiper.recommendSwiper.swipeNext();
-	  });
-
-    //推荐商品图片-响应式
-    $(".js_recPic").each(function(){
-      $(this).oPicture({
-          //自定义节点宽度
-          //sm:544,md:700,lg:992,xl:1200,
-      }).init();
-    });
-
-
-    swiper.entrabceSwiper = new Swiper ('.js_swiperEntrabce', {
-
+	/**
+	 * banner
+	 */
+	//banner轮播
+  var screenWidth = document.body.offsetWidth;
+  if (screenWidth <= 991) {
+    swiper.bannerSwiper = new Swiper('.js_bannerSwiper',{
       loop: true,
-      // autoplay: 3000,
-      calculateHeight : true,
+      autoplay: 3000,
+      slidesPerView: 3,
+      watchActiveIndex: true,
+      updateOnImagesReady: true,
+      centeredSlides: true,
       onSlideChangeEnd: function(swiper){
         var index = swiper.activeIndex;
-        var imgSrc = $('.js_swiperEntrabce .swiper-slide').eq(index).find('img').attr('src');
-        $('.js_recImgSmall').attr('src',imgSrc);
+
+        // $('.js_bannerSwiper .swiper-slide-active').find('img').css({
+        //   'height': 500,
+        //   'margin-left': -455.5,
+        //   'margin-top': -250
+        // });
+        
+        $('.js_bannerSwiper .swiper-slide-active').find('img').animate({
+          'height': 500,
+          'margin-left': -455.5,
+          'margin-top': -250
+        }, 500);
+
+        $('.js_bannerSwiper .swiper-slide').not('.swiper-slide-active').find('img').css({
+          'height': 382,
+          'margin-left': -348.5,
+          'margin-top': -191
+        });
       }
     });
+    $(".js_bg").oBgCover().init();
 
-    $('.js_swiperEntrabcePrev').on('click', function(e){
-      e.preventDefault();
-      swiper.entrabceSwiper.swipePrev();
-    });
-    $('.js_swiperEntrabceNext').on('click', function(e){
-      e.preventDefault();
-      swiper.entrabceSwiper.swipeNext();
+    $('.js_bannerSwiper .swiper-slide').not('.swiper-slide-active').find('img').css({
+      'height': 382,
+      'margin-left': -348.5,
+      'margin-top': -191
     });
 
+    $('.js_bannerSwiper .swiper-slide-active').find('img').css({
+      'height': 500,
+      'margin-left': -455.5,
+      'margin-top': -250
+    });
+
+    // swiper.bannerSwiper.
+  } else {
+    swiper.bannerSwiper = new Swiper('.js_bannerSwiper',{
+      loop: true,
+      autoplay: 3000,
+
+      onSlideChangeEnd: function(swiper){
+        var index = swiper.activeIndex;
+        console.log(index);
+      }
+  	});
+  }
+
+	setTimeout(function(){
+
+		//banner左侧高度跟随
+    $('.js_bannerInfo').oHrel({
+      obj:'$(this).next()'
+    }).init();
+    //banner左侧文字居中
+    $(".js_bannnerCenter").oBoxCenter().init();
+
+	},1000);
 
 
-	},2000);
+	/**
+	 * 产品推荐
+	 */
+	
+	//产品推荐轮播
+  swiper.recommendSwiper = new Swiper ('.js_swiperRecMain', {
+
+    loop: true,
+    autoplay: 3000,
+
+    onSlideChangeEnd: function(swiper){
+    	var index = swiper.activeIndex;
+    	var imgSrc = $('.js_swiperRecMain .swiper-slide').eq(index).find('img').attr('src');
+      $('.js_recImgSmall').attr('src',imgSrc);
+    }
+	});
+	$('.js_swiperRecMainPrev').on('click', function(e){
+  	e.preventDefault();
+    swiper.recommendSwiper.swipePrev();
+  });
+  $('.js_swiperRecMainNext').on('click', function(e){
+    e.preventDefault();
+    swiper.recommendSwiper.swipeNext();
+  });
+
+  //推荐商品图片-响应式
+  $(".js_recPic").each(function(){
+    $(this).oPicture({
+        //自定义节点宽度
+        //sm:544,md:700,lg:992,xl:1200,
+    }).init();
+  });
+  swiper.recommendSwiper.reInit();
+
+
+  /**
+   * 首页入口
+   */
+  swiper.entrabceSwiper = new Swiper ('.js_swiperEntrabce', {
+
+    loop: true,
+    autoplay: 3000,
+    calculateHeight : true,
+    autoplayDisableOnInteraction : false,//用户操作之后，不禁止autoplay
+    updateOnImagesReady : true,
+    onSlideChangeStart: function(swiper){
+      var index = swiper.activeIndex;
+      $('.js_swiperEntrabce .swiper-slide').find('.entrance-fontbox').css({
+        'filter':'alpha(opacity=0)',  
+			  '-moz-opacity':'0',
+			  '-khtml-opacity': '0',
+			  'opacity': '0'
+      });
+      $('.js_swiperEntrabce .swiper-slide').eq(index).find('.entrance-fontbox').animate({
+        'filter':'alpha(opacity=1)',  
+			  '-moz-opacity':'1',
+			  '-khtml-opacity': '1',  
+			  'opacity': '1'
+      }, 1000);
+    }
+  });
+
+  if (screenWidth <= 700) {
+  	swiper.entrabceSwiper.params.slidesPerView = 2;
+    swiper.entrabceSwiper.params.watchActiveIndex = true;
+    swiper.entrabceSwiper.params.calculateHeight = true;
+  } else {
+  	swiper.entrabceSwiper.params.slidesPerView = 1;
+    swiper.entrabceSwiper.params.calculateHeight = true;
+  }
+
+  $('.js_swiperEntrabcePrev').on('click', function(e){
+    e.preventDefault();
+    swiper.entrabceSwiper.swipePrev();
+  });
+  $('.js_swiperEntrabceNext').on('click', function(e){
+    e.preventDefault();
+    swiper.entrabceSwiper.swipeNext();
+  });
+
+  swiper.entrabceSwiper.reInit();
+
+
+
+	// },2000);
 
 
 
@@ -147,8 +186,10 @@ $(function() {
       swiper.entrabceSwiper.params.updateOnImagesReady = true;
     } else {
       swiper.entrabceSwiper.params.slidesPerView = 1;
-      swiper.entrabceSwiper.reInit();
+      // swiper.entrabceSwiper.reInit();
     }
+
+    swiper.entrabceSwiper.reInit();
 
 
     if (screenWidth <= 991) {
@@ -159,15 +200,19 @@ $(function() {
       swiper.bannerSwiper.params.updateOnImagesReady = true;
       swiper.bannerSwiper.params.onSlideChangeEnd = function(swiper){
           var index = swiper.activeIndex;
-          // var imgSrc = $('.js_swiperRecMain .swiper-slide').eq(index).find('img').attr('src');
-          // $('.js_recImgSmall').attr('src',imgSrc);
 
-
-          $('.js_bannerSwiper .swiper-slide-active').find('img').css({
+          // $('.js_bannerSwiper .swiper-slide-active').find('img').css({
+          //   'height': 500,
+          //   'margin-left': -455.5,
+          //   'margin-top': -250
+          // });
+          //动画
+          $('.js_bannerSwiper .swiper-slide-active').find('img').animate({
             'height': 500,
             'margin-left': -455.5,
             'margin-top': -250
-          });
+          }, 500);
+          
 
           $('.js_bannerSwiper .swiper-slide').not('.swiper-slide-active').find('img').css({
             'height': 382,
@@ -202,8 +247,6 @@ $(function() {
       swiper.bannerSwiper.reInit();
     }
 
-
-    swiper.entrabceSwiper.reInit();
 
   });
 });
