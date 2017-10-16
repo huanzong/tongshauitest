fda921b067fa27ac89ac77edf009a86d55a63733
刘悦 <liu.yue@trs.com.cn>
2017-10-16 15:54:46 +0800

    绑定手机

--- leader_site/images/member_bindmob.js
+++ leader_site/images/member_bindmob.js
@@ -23,9 +23,8 @@ $(function(){
 
 
 //验证手机号
-    $('.js-newMobile').blur(function(){
 
-    $('.js-bindmobinput').blur(function(){
+    $('.js-newMobile').blur(function(){
         var yanzhengtrue = $(this).siblings('.Validform_checktip').hasClass('Validform_right');
         if(yanzhengtrue){
             $('.js-getinfo').removeClass('l-btn-disable').click(function(){
@@ -171,9 +170,12 @@ $(function(){
 
             //禁止点击
             $('.js-submintData').unbind().bind('click',function(){
+
                 return false;
             })
         }
+
+    })
 
 
 
