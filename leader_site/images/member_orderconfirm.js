b156affcbcb305968bfe039979b309b387cc3563
wangfei <wang.fei@trs.com.cn>
2017-10-19 16:40:31 +0800

    添加订单确认页面

---
+++ leader_site/images/member_orderconfirm.js
@@ -0,0 +1,7 @@
+/**
+ * Created by 15610 on 2017/10/19.
+ */
+$("#js_orderConfirmSave").oSelect().init();
+$("#js_orderConfirmCity").oSelect().init();
+$("#js_orderConfirmArea").oSelect().init();
+$("#js_orderConfirmRode").oSelect().init();
