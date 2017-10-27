f9c96e67087e5a4d55f51151497bd7c70fe3f7bf
wangfei <wang.fei@trs.com.cn>
2017-10-27 10:43:29 +0800

    修改绑定手机邮箱头部

--- leader_site/images/templet_member_bindmob.js
+++ leader_site/images/templet_member_bindmob.js
@@ -10,7 +10,7 @@ $(function(){
     //     jumpToLoginPage()；
     // }
 
-    $('.js-memberRevRateLine').css('width','25%');
+
     //页面加载时调个人信息
     $.ajax({
         type: "get",
@@ -98,9 +98,10 @@ $(function(){
                 success: function(returnData){
                     if (jQuery.trim(returnData).length > 0) {
                         if (jQuery.trim(returnData).indexOf("200")>-1) {
-                            $('.js-memberRevRateLine').css('width','75%');
-                            $('.js-memberRevRateTree').addClass('member-revisemob-No2').children('.member-revisemob-line-point02').children('div').addClass('.member-revisemob-line-finishpoint');
-                            $('.js-memberRevRateTree').children('.member-revisemob-line-point03').children('div').addClass('.member-revisemob-line-finishpoint');
+                            $('.js_memberRevisThree').addClass('member-revisemob-two').removeClass('member-revisemob-thres').removeClass('member-revisemob-one');
+                            //$('.js-memberRevRateLine').css('width','75%');
+                            //$('.js-memberRevRateTree').addClass('member-revisemob-No2').children('.member-revisemob-line-point02').children('div').addClass('.member-revisemob-line-finishpoint');
+                            //$('.js-memberRevRateTree').children('.member-revisemob-line-point03').children('div').addClass('.member-revisemob-line-finishpoint');
                             $('.js-validateEmail').hide();
                             $('.js-bindNewMob').show();
                         }
@@ -211,8 +212,9 @@ $(function(){
                     success: function(returnData){
                         if (jQuery.trim(returnData).length > 0) {
                             if (jQuery.trim(returnData).indexOf("200")>-1) {
-                                $('.js-memberRevRateLine').css('width','100%');
-                                $('.js-memberRevRateTree').addClass('member-revisemob-No3').children('.member-revisemob-line-point03').children('div').addClass('.member-revisemob-line-finishpoint');
+                                $('.js_memberRevisThree').addClass('member-revisemob-thres').removeClass('member-revisemob-two').removeClass('member-revisemob-one');
+                                //$('.js-memberRevRateLine').css('width','100%');
+                                //$('.js-memberRevRateTree').addClass('member-revisemob-No3').children('.member-revisemob-line-point03').children('div').addClass('.member-revisemob-line-finishpoint');
                                 $('.js-bindNewMob').hide();
                                 $('.js-bingsuccess').show();
                                 document.cookie="isAlterBind=1;path=/";
