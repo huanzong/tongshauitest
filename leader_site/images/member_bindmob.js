8943f918ff94c820caff41a625a54dadd0f82c48
wangfei <wang.fei@trs.com.cn>
2017-11-06 12:23:14 +0800

    调整绑定手机静态页

--- leader_site/images/member_bindmob.js
+++ leader_site/images/member_bindmob.js
@@ -1,3 +1,31 @@
 /**
  * Created by 15610 on 2017/11/6.
  */
+
+
+$("#js_unbindmob").attr('autotext',"邮箱（"+'1511***@163.com'+"）");
+$("#js_unbindmob").append("<option value='1'>手机（"+15115151515+"）</option>");
+$("#js_unbindmob").append("<option value='2'>手机（"+18616161616+"）</option>");
+$("#js_unbindmob").oSelect().init();
+
+
+
+$('.js-sendemailcode').click(function(){
+    btnTimeOut($(this),120,'内有效');
+});
+$('.js-getinfo').click(function(){
+    btnTimeOut($(this),120,'内有效');
+});
+
+
+$('.js_subimGetUp').click(function(){
+    $('.js-validateEmail').hide();
+    $('.js_memberRevisThree').removeClass('member-revisemob-one').addClass('member-revisemob-two');
+    $('.js-bindNewMob').show();
+})
+
+$('.js-submintData').click(function(){
+    $('.js-bindNewMob').hide();
+    $('.js_memberRevisThree').removeClass('member-revisemob-two').addClass('member-revisemob-three');
+    $('.js-bingsuccess').show();
+})
--- leader_site/images/member_revisemail.js
+++ leader_site/images/member_revisemail.js
@@ -1,3 +1,8 @@
 /**
  * Created by 15610 on 2017/11/6.
  */
+    //静态页面测试添加，正常后隐藏
+$("#js_revisemail").attr('autotext',"邮箱（"+'151151515115115151515151151515155@163.com'+"）");
+$("#js_revisemail").append("<option value='1'>手机（"+15115151515+"）</option>");
+$("#js_revisemail").append("<option value='2'>手机（"+18616161616+"）</option>");
+$("#js_revisemail").oSelect().init();
