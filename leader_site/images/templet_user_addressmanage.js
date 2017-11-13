43941685ab58888717b26fc1ac00306b2adb338a
zhangjing <1931356942@qq.com>
2017-11-13 14:34:43 +0800

    地址管理js调整

--- leader_site/images/templet_user_addressmanage.js
+++ leader_site/images/templet_user_addressmanage.js
@@ -212,9 +212,7 @@ function saveUserAddress(){
     $.ajax({
         url:siteConfig.userUrl+"/hshop-user/front/userRegion/save",
         type:"post",
-        dataType:"json",
         data:data,
-        applicationType:true,
         login:true,
         success_cb:function(responseT){
             if(responseT.isSuccess){
