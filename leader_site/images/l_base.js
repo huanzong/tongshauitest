873379064048489c60a24ac94d5fcbeb0ec2b6c7
宫磊 <gong.lei@trs.com.cn>
2017-11-14 17:14:53 +0800

    将l_base中搜索栏弹层部分注释,实时检索JS输入框默认内容拦截。

--- leader_site/images/l_base.js
+++ leader_site/images/l_base.js
@@ -156,79 +156,79 @@ $(function () {
     });
 
     //搜索历史
-    $('.js_searchHistory').bind('input propertychange', function () {
-        var screenWidth = document.body.offsetWidth;
-        if ($(this).val()) {
-            if (screenWidth > 1199) {
-                $('.js_searchBox_xl').show();
-            } else {
-                $('.js_searchBox_lg').show();
-            }
-            $('.js_searchBoxQuick_lg').hide();
-        } else {
-            $('.js_searchBox').hide();
-            $('.js_searchBoxQuick_lg').show();
-        }
-    }).on('blur', function () {
-        $('.js_searchBox').hide();
-        $('.js_searchBoxQuick_lg').show();
-    }).on('focus', function () {
-        if ($(this).val()) {
-            if (screenWidth > 1199) {
-                $('.js_searchBox_xl').show();
-            } else {
-                $('.js_searchBox_lg').show();
-            }
-            $('.js_searchBoxQuick_lg').hide();
-        }
-    });
-
-    //搜索--lg
-    $('.js_search_lg').on('click', function () {
-        $('.js_navSearchLgHide')
-            .removeClass('o_lg-show')
-            .removeClass('o_md-show')
-            .removeClass('o_sm-show')
-            .removeClass('o_xs-show');
-        $('.js_navSearchLgHide').hide();
-        $('.js_navSearchLg')
-            .addClass('o_lg-show')
-            .addClass('o_md-show')
-            .addClass('o_sm-show')
-            .addClass('o_xs-show');
-        $('.js_navSearchLg').show();
-    });
-
-    //关闭搜索
-    $('.js_navSearchClose').on('click', function () {
-
-        $('.js_navSearchLg')
-            .removeClass('o_lg-show')
-            .removeClass('o_md-show')
-            .removeClass('o_sm-show')
-            .removeClass('o_xs-show');
-        $('.js_navSearchLg').hide();
-
-        $('.js_navSearchLgHide')
-            .addClass('o_lg-show')
-            .addClass('o_md-show')
-            .addClass('o_sm-show')
-            .addClass('o_xs-show');
-        $('.js_navSearchLgHide').show();
-        $('.js_navSearchLgHide.js_ignore').removeClass('o_lg-show').hide();
-
-    });
-
-    //展示导航菜单
-    $('.js_menuShow').on('click', function () {
-        if ($(this).hasClass('icon-menu')) {
-            $(this).removeClass('icon-menu').addClass('icon-close');
-            $('.js_navMdShow').show();
-        } else {
-            $(this).removeClass('icon-close').addClass('icon-menu');
-            $('.js_navMdShow').hide();
-        }
-    });
+    // $('.js_searchHistory').bind('input propertychange', function () {
+    //     var screenWidth = document.body.offsetWidth;
+    //     if ($(this).val()) {
+    //         if (screenWidth > 1199) {
+    //             $('.js_searchBox_xl').show();
+    //         } else {
+    //             $('.js_searchBox_lg').show();
+    //         }
+    //         $('.js_searchBoxQuick_lg').hide();
+    //     } else {
+    //         $('.js_searchBox').hide();
+    //         $('.js_searchBoxQuick_lg').show();
+    //     }
+    // }).on('blur', function () {
+    //     $('.js_searchBox').hide();
+    //     $('.js_searchBoxQuick_lg').show();
+    // }).on('focus', function () {
+    //     if ($(this).val()) {
+    //         if (screenWidth > 1199) {
+    //             $('.js_searchBox_xl').show();
+    //         } else {
+    //             $('.js_searchBox_lg').show();
+    //         }
+    //         $('.js_searchBoxQuick_lg').hide();
+    //     }
+    // });
+    //
+    // //搜索--lg
+    // $('.js_search_lg').on('click', function () {
+    //     $('.js_navSearchLgHide')
+    //         .removeClass('o_lg-show')
+    //         .removeClass('o_md-show')
+    //         .removeClass('o_sm-show')
+    //         .removeClass('o_xs-show');
+    //     $('.js_navSearchLgHide').hide();
+    //     $('.js_navSearchLg')
+    //         .addClass('o_lg-show')
+    //         .addClass('o_md-show')
+    //         .addClass('o_sm-show')
+    //         .addClass('o_xs-show');
+    //     $('.js_navSearchLg').show();
+    // });
+    //
+    // //关闭搜索
+    // $('.js_navSearchClose').on('click', function () {
+    //
+    //     $('.js_navSearchLg')
+    //         .removeClass('o_lg-show')
+    //         .removeClass('o_md-show')
+    //         .removeClass('o_sm-show')
+    //         .removeClass('o_xs-show');
+    //     $('.js_navSearchLg').hide();
+    //
+    //     $('.js_navSearchLgHide')
+    //         .addClass('o_lg-show')
+    //         .addClass('o_md-show')
+    //         .addClass('o_sm-show')
+    //         .addClass('o_xs-show');
+    //     $('.js_navSearchLgHide').show();
+    //     $('.js_navSearchLgHide.js_ignore').removeClass('o_lg-show').hide();
+    //
+    // });
+    //
+    // //展示导航菜单
+    // $('.js_menuShow').on('click', function () {
+    //     if ($(this).hasClass('icon-menu')) {
+    //         $(this).removeClass('icon-menu').addClass('icon-close');
+    //         $('.js_navMdShow').show();
+    //     } else {
+    //         $(this).removeClass('icon-close').addClass('icon-menu');
+    //         $('.js_navMdShow').hide();
+    //     }
+    // });
 
     userLoginStatus();
 
