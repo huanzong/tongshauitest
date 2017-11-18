476ebebc6027d5a6a10ddd5d8055775d275c5c05
zhangjing <1931356942@qq.com>
2017-11-18 16:05:26 +0800

    详情页对比页浮层显示隐藏

--- leader_site/images/templet_product_detail_compare.js
+++ leader_site/images/templet_product_detail_compare.js
@@ -526,12 +526,14 @@ function deleteAllCompareItem() {
 //显示对比浮层
 function showCompareFlowLayout(){
     $('.prolist-compare').show();
+    $('.js_compareBox').show();
     $('.js_compareBoxShow').attr('data-show', 1);
     $('.js_compareBoxShow').html('隐藏<i class="iconfont icon-arrow-line-down"></i>');
 }
 //隐藏对比浮层
 function hideCompareFlowLayout(){
     $('.prolist-compare').hide();
+    $('.js_compareBox').hide();
     $('.js_compareBoxShow').attr('data-show', 0);
     $('.js_compareBoxShow').html('展开<i class="iconfont icon-arrow-line-up"></i>');
 }
