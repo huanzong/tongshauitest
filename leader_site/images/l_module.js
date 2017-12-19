8a54c2393ea6acd27cd1f78199e9495fc9fb9861
wangfei <wang.fei@trs.com.cn>
2017-12-19 18:38:42 +0800

    修改样式

--- leader_site/images/l_module.js
+++ leader_site/images/l_module.js
@@ -226,7 +226,7 @@ $(function() {
                         $this.css('color','#666');
                         //表单校验时触发
                         if(!$this.attr('phtype')){
-                            //$this.hasClass('Validform_error')?$this.css('border','1px solid #f39800'):$this.css('border','1px solid #ccc');
+                            $this.hasClass('Validform_error')?$this.css('border','1px solid #f39800'):$this.css('border','1px solid #ccc');
                             // $this.css('border','1px solid #ccc');
                         }
                     },300);
@@ -235,7 +235,7 @@ $(function() {
                     if(!$this.attr('data-normal')){
                         $this.validHideError();
                     }
-                    $this.css('border','1px solid #e60012');
+                    //$this.css('border','1px solid #e60012');
                 });
             }
         },
