60bc766b645d75b6a200b5006746c775b2a7da88
wangfei <wang.fei@trs.com.cn>
2017-12-19 15:10:59 +0800

    修改裁切控件

--- leader_site/images/member_personal.js
+++ leader_site/images/member_personal.js
@@ -267,7 +267,7 @@ $(function(){
                         }else{
                             $('.js-rightimg').attr('src',templet_pic).css({'height':photoBoxWidth*0.85,'width':photoBoxWidth*0.85})
                             $(".jcrop-preview").attr("src",templet_pic);
-
+                            $('.js_selectWf').attr("src",templet_pic);
                             //jcrop_api.setImage(templet_pic, function(){
                                 jcrop_api.setOptions({
                                     //outerImage: templet_pic,
