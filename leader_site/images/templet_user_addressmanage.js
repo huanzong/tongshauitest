bc3ae13d804ae29cb513454c54e7c7b5b29c2791
zhangjing <1931356942@qq.com>
2017-12-20 11:33:26 +0800

    地址管理页设置hover

--- leader_site/images/templet_user_addressmanage.js
+++ leader_site/images/templet_user_addressmanage.js
@@ -320,7 +320,7 @@ function saveUserAddress(){
                 resetForm();//重置表单
                 $(".js_form_addAddrManagement").hide();
                 templet_isSubmiting=false;
-                /*globalShade2("添加成功",1,2000);*/
+                globalShade2("添加地址成功",2,2000);
             }else{//添加地址失败
                 globalShade2(responseT.resultMsg,2,2000);
                 templet_isSubmiting=false;
@@ -907,7 +907,7 @@ function updateUserAddress(){
     return bool;
 }
 $(function(){
-    $('.o_Dropdown ').mouseout(function(){return });//省市区去掉鼠标移除事件
+    $('.o_Dropdown ').hover(function(){return },function(){return })//省市区去掉鼠标移除事件
 })
 
 
