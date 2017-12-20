f0948544b60e535200e77ac854fdd145b6b4572b
zhangjing <1931356942@qq.com>
2017-12-20 11:38:35 +0800

    地址管理页设置hover

--- leader_site/images/templet_user_addressmanage.js
+++ leader_site/images/templet_user_addressmanage.js
@@ -13,8 +13,7 @@ var templet_pageSize=10;
 var templet_isSubmiting=false;
 loadUserInfoList();//获取用户地址列表
 
-
-
+$('.o_Dropdown ').hover(function(){return },function(){return })//省市区去掉鼠标移除事件
 
 var infotell=[];
 //固定电话号码错误显示逻辑
@@ -906,9 +905,7 @@ function updateUserAddress(){
 
     return bool;
 }
-$(function(){
-    $('.o_Dropdown ').hover(function(){return },function(){return })//省市区去掉鼠标移除事件
-})
+
 
 
 
